57036cbb3c13e7c78ceab4953fa51bb1058f3148
Luis Pires <luisbpires@hotmail.com>
2020-01-05 19:06:25 +0000

    added navbar template

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -53,7 +53,73 @@
       
     </v-row>
 
-   
+    <div class="navBar">
+      
+      <v-navigation-drawer
+      color="#5C6BC0"
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      fixed
+      permanent
+      height="800"
+      >
+    <v-list-item>
+          <v-list-item-title>NavBar Template</v-list-item-title>
+  
+          <v-btn
+            icon
+            @click.stop="mini = !mini"
+          >
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+  
+        <v-divider></v-divider>
+  
+        <v-list dense>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+  
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+  </v-navigation-drawer>
+
     </div>
+  </div>
 </template>
 
+<style>
+.navBar{
+  top: 0px;
+  
+}
+</style>
+
+<script>    
+
+export default {
+  name: "login",
+
+  data: () => ({
+      drawer: true,
+      items: [
+        { title: 'Home', icon: 'mdi-home-city' },
+        { title: 'My Account', icon: 'mdi-account' },
+        { title: 'Users', icon: 'mdi-account-group-outline' },
+      ],
+     mini: true, 
+    
+  })
+};
+
+</script>
