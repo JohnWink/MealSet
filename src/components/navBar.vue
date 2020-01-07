0adf0e080ac358d81232f34a85c60c4a074ceced
Luis Pires <luisbpires@hotmail.com>
2020-01-07 21:56:50 +0000

    updating navbar

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,45 +1,175 @@
 <template>
       <div class="navBar">
-      
-      <v-navigation-drawer
-      color="#5C6BC0"
-      v-model="drawer"
-      :mini-variant.sync="mini"
-      fixed
-      permanent
-      height="100%"
+<!--++++++++++++++++++++++++++++++++++++Retracted NavBar+++++++++++++++++++++++++++++-->
+        <v-navigation-drawer
+        fixed
+        height="100%"
+        v-model="drawer"
+        :mini-variant="mini"
+        permanent
+        color ="#5C6BC0"
       >
-    <v-list-item>
-          <v-list-item-title>NavBar Template</v-list-item-title>
+      
+            <v-btn
+              width="100%"
+              @click.stop="drawer = !drawer">
+              <v-icon>mdi-chevron-right</v-icon>
+            </v-btn>
+          <template v-slot:prepend>
+          <v-list>
+            <v-list-item class="logoColor">
+             <v-img src="../assets/icon_V2_logo2.png">
+               
+             </v-img>
+            </v-list-item>
   
-          <v-btn
-            icon
-            @click.stop="mini = !mini"
-          >
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-        </v-list-item>
+          
+          </v-list>
+        </template>
   
         <v-divider></v-divider>
   
-        <v-list dense>
-          <v-list-item
-            v-for="item in items"
-            :key="item.title"
-            link
-          >
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item link>
             <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
+              <v-icon>fas fa-store</v-icon>
             </v-list-item-icon>
-  
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
+            <v-list-item-title>Restaurantes</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>fas fa-utensils</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Pratos</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>fas fa-user-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Perfil</v-list-item-title>
           </v-list-item>
         </v-list>
+      </v-navigation-drawer>
 
-  </v-navigation-drawer>
+<!--+++++++++++++++++++++++++++++++++++++++++++++Expanded NavBar++++++++++++++++++++++++++++++++++++++++++-->
+       <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        bottom
+        temporary
+        color ="#5C6BC0"
+      >
+       <v-btn
+              width="100%"
+              @click.stop="drawer = !drawer">
+              <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
 
+        <template v-slot:prepend>
+          <v-list>
+            <v-list-item class="logoColor">
+             <v-img
+             
+              max-height="49" 
+              max-width="49"
+              position=center
+             src="../assets/icon_V2_logo2.png">
+             </v-img>
+            </v-list-item>
+  
+           
+          </v-list>
+        </template>
+  
+        <v-divider></v-divider>
+  
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>fas fa-store</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Restaurantes</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>fas fa-utensils</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Pratos</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>fas fa-user-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Perfil</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+  
+
+      <!--
+       <v-navigation-drawer
+        expand-on-hover
+        fixed
+        permanent
+        height="100%"
+        color ="#5C6BC0"
+      >
+        <template v-slot:prepend>
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+              </v-list-item-avatar>
+            </v-list-item>
+  
+            <v-list-item
+              link
+              two-line
+            >
+              <v-list-item-content>
+                <v-list-item-title class="title">Sandra Adams</v-list-item-title>
+                <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-icon>mdi-menu-down</v-icon>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </template>
+  
+        <v-divider></v-divider>
+  
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>fas fa-store</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Restaurantes</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>fas fa-utensils</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Pratos</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>fas fa-user-circle</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Perfil</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+-->
     </div>
   
 </template>
@@ -62,6 +192,9 @@ export default {
 </script>
 
 <style>
+.logoColor{
+  background-color: yellow
+}
 .navBar{
   top: 0px;
   
