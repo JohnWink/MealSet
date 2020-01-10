3617b8e4dc149fb024c8c34a2d83f536b99ddbcb
ANA SOFIA RIBEIRO SANTOS MOREIRA <9180120@ESEIG.PT>
2020-01-10 17:54:19 +0000

    cards

---
+++ src/components/introductionCards.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="introductionCards">
+    <v-app id="inspire" class="pl-12 ml-12">
+      <v-spacer></v-spacer>
+      <v-title class="text-center">Bem vindo ao MealSet!</v-title>
+      <v-title class="text-center mt-6">Começa já a utilizar a nossa aplicação</v-title>
+      <v-spacer></v-spacer>
+      <v-row>
+        <v-col cols="6" md="4" v-for="titulo1 in titulos1" :key="titulo1">
+          <v-card class="mx-auto" elevation="10" height="300" max-width="400">
+            <v-title>{{ titulo1 }}</v-title>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6" md="4" v-for="titulo2 in titulos2" :key="titulo2">
+          <v-card class="mx-auto" elevation="10" height="300" max-width="400">
+            <v-title>{{ titulo2 }}</v-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-app>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: "introductionCards",
+  data: () => ({
+    titulos1: ["mano", "fixe", "lol"],
+    titulos2: ["bro", "top", "mana"]
+  })
+};
+</script>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,75 +1,51 @@
 
 <template>
-
-    <div class="LandingPage">
-  
+  <div class="LandingPage">
     <v-row align="center" justify="center">
-      <v-img
-      src="../assets/landingPageImg.jpg"
-      class="grey lighten-3"
-      >
-      <v-row
-      align="start"
-      justify="center"
-      >
-         <!--Registration Button to pop up modal   !-->
-         <v-col md="1">
+      <v-img src="../assets/landingPageImg.jpg" class="grey lighten-3">
+        <v-row align="start" justify="center">
+          <!--Registration Button to pop up modal   !-->
+          <v-col md="1">
             <navBar/>
-         </v-col>
-         <!-- ++++++++++++++++++++++++++++++++++Registration dialog box+++++++++++++++++++++++++++++++-->
-    
-        <v-col  
-          md="1"
-          offset-sm="6">
-          
-           <registration/>
-     
-          
-        </v-col>
-        <!--Login Button to pop up modal   !-->
-        <v-col  
-          md="2">
-          <login/>
-        </v-col>
-        <!--Search Text Field   !-->
-        <v-col  md="2">
-          <v-text-field
-            prepend-inner-icon="fas fa-search"
-            height="2px"
-            background-color= "#FFFFFF"
-            filled
-            rounded
-            dense
-          >
-      
-          </v-text-field>
-        </v-col>
-      </v-row>
-       
+          </v-col>
+          <!-- ++++++++++++++++++++++++++++++++++Registration dialog box+++++++++++++++++++++++++++++++-->
+
+          <v-col md="1" offset-sm="6">
+            <registration/>
+          </v-col>
+          <!--Login Button to pop up modal   !-->
+          <v-col md="2">
+            <login/>
+          </v-col>
+          <!--Search Text Field   !-->
+          <v-col md="2">
+            <v-text-field
+              prepend-inner-icon="fas fa-search"
+              height="2px"
+              background-color="#FFFFFF"
+              filled
+              rounded
+              dense
+            ></v-text-field>
+          </v-col>
+        </v-row>
       </v-img>
-      
     </v-row>
 
-
-
-
+    <introductionCards/>
     <footerVue/>
-
   </div>
-  
-  
 </template>
 
 <style>
-
 </style>
 
-<script>    
-
+<script>
 import NavBar from "@/components/navBar.vue";
 import footerVue from "@/components/footerVue.vue";
 import registration from "@/components/registration.vue";
 import login from "@/components/login.vue";
+import introductionCards from "@/components/introductionCards.vue";
 
 export default {
   name: "landingPage",
@@ -77,13 +53,9 @@ export default {
     NavBar,
     footerVue,
     registration,
-    login
-
+    login,
+    introductionCards
   },
-  data:() =>({
-  
-  })
-
+  data: () => ({})
 };
-
 </script>
