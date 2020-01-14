e9776c117ffcdf0638270e97cbcb99ac17bfa7ed
Miguel Amaral <mikefirebun@gmail.com>
2020-01-14 17:53:49 +0000

    working on the comment section, managed to get the starts working and setting componets

--- src/components/commentReview.vue
+++ src/components/commentReview.vue
@@ -1,11 +1,35 @@
-<template >
-  <p>comment section in progress</p> <!--just to test if it picks up-->
+<template>    
+    
+    <v-container>
+        <v-row id="comment" v-for="comment in comments" :key="comment" justify="space-between" align="center" :style="checker">
+            <v-col cols="4" class="mx-4">{{comment.name}} {{comment.date}}</v-col>
+
+            <!--method to calculate and print the rate stars and empthy stars-->
+            <v-col  cols="2">
+                <div v-html="rateprint(comment.evaluation)"></div>
+                
+                
+
+
+                
+            </v-col>
+        
 
 
 
-
-
+        </v-row>
+    </v-container>
 </template>
+
+
+<style>
+    #comment{
+        border-radius: 20px;
+
+    }
+</style>
+
+
 
 
 <script>
@@ -34,8 +58,33 @@ export default {
             date:"3/7/2020",
             evaluation: 1
         },
-      ]
+      ],
+      checker:"border: solid red"
+      
 
-  })
+
+    }),
+    methods:{
+        rateprint: function(rate){
+            
+            let rateCode = ""
+            for(let i=0;i<rate;i++){
+                rateCode += `<i style="color:gold" class="fas fa-star"></i> `
+
+            }      
+            
+            if (rate<5){
+
+                for(let i=0;i<rate-5;i++){
+                rateCode += `<i class="fas fa-star"></i> `
+
+                } 
+            }
+
+            return rateCode;
+            
+        }
+
+    },
 };
 </script>
