95ff34b22f7d2b543b87a62539bd1c7ef3c62af7
Ana Moreira <9180120@esmad.ipp.pt>
2020-01-18 23:21:10 +0000

    a trabalhar na modal de reserva

--- src/components/Reservation.vue
+++ src/components/Reservation.vue
@@ -1,19 +1,60 @@
 <template>
-   <v-dialog v-model="dialog" persistent max-width="600px">
+  <v-dialog v-model="dialog" persistent max-width="500">
     <template v-slot:activator="{ on }">
-          <v-btn large rounded color="#f7c23e" dark v-on="on">Fazer Reserva</v-btn>
+      <v-btn large rounded color="#f7c23e" dark v-on="on">Fazer Reserva</v-btn>
     </template>
+    <!--MODAL DE RESERVA-->
     <v-card>
+      <!--Fechar-->
+      <v-toolbar color="#f7c23e">
+        <v-row>
+          <v-col class="text-left mt-2 ml-6">
+            <v-toolbar-title id="text">Fazer reserva</v-toolbar-title>
+          </v-col>
+          <v-col class="text-right mt-2">
+            <v-btn color="#ffffff" text right @click="dialog = false">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-toolbar>
+
       <v-form class="pa-10" ref="form" v-model="valid" lazy-validation>
-        <v-btn color="blue darken-1" text @click="dialog = false">X</v-btn>
+        <!--Nome-->
+        <v-text-field
+          prepend-icon="fas fa-user-circle"
+          v-model="name"
+          :rules="nameRules"
+          label="Nome"
+          required
+          clearable
+          color="#5C6BC0"
+        ></v-text-field>
 
-        <v-text-field v-model="name" :rules="nameRules" label="Nome"></v-text-field>
+        <!--Nr de pessoas-->
+        <v-text-field
+          prepend-icon="fas fa-users"
+          v-model="peopleNumber"
+          :rules="peopleNumberRules"
+          label="Número de convidados"
+          required
+          clearable
+          color="#5C6BC0"
+        ></v-text-field>
 
-        <v-text-field v-model="peopleNumber"  :rules="peopleNumberRules" label="Número de convidados"></v-text-field>
+        <!--Qual refeição-->
+        <v-select
+          prepend-icon="fas fa-utensils"
+          v-model="mealTimeSelection"
+          :items="mealTime"
+          :rules="mealTimeRules"
+          label="Qual vai ser sua refeição?"
+          required
+          color="#5C6BC0"
+        ></v-select>
 
-          <v-select v-model="mealTimeSelection" :items="mealTime" :rules="mealTimeRules" label="Qual vai ser sua refeição?"></v-select>
-<!--+++++++++++++++++++++++++++++++++++++++++++Secção da data+++++++++++++++++++++++++++++++++++++++++++++++++-->
-         <v-layout row wrap>
+        <!--+++++++++++++++++++++++++++++++++++++++++++Secção da data+++++++++++++++++++++++++++++++++++++++++++++++++-->
+        <v-layout row wrap>
           <v-menu
             v-model="fromDateMenu"
             :close-on-content-click="false"
@@ -25,27 +66,28 @@
             max-width="290px"
             min-width="290px"
           >
-      <template v-slot:activator="{ on }">
-        <v-text-field
-          label="Data da reserva"
-          readonly
-          :value="fromDateDisp"
-          v-on="on"
-          :rules="mealDateRules"
-        ></v-text-field>
-      </template>
-      <v-date-picker
-        locale="en-in"
-        :min="minDate"
-        :max="maxDate"
-        v-model="fromDateVal"
-        no-title
-        @input="fromDateMenu = false"
-      ></v-date-picker>
-    </v-menu>
-  </v-layout>
-
-
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                prepend-icon="far fa-calendar-alt"
+                label="Data da reserva"
+                readonly
+                :value="fromDateDisp"
+                v-on="on"
+                :rules="mealDateRules"
+                required
+                color="#5C6BC0"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              locale="en-in"
+              :min="minDate"
+              :max="maxDate"
+              v-model="fromDateVal"
+              no-title
+              @input="fromDateMenu = false"
+            ></v-date-picker>
+          </v-menu>
+        </v-layout>
 
         <!--  Someday....
         <v-file-input
@@ -56,99 +98,87 @@
           v-model="choosenImg"
         ></v-file-input>
         -->
-      
- 
-      <v-btn color="primary" text @click="reset">Limpar Informação </v-btn>
-
-      <v-btn color="primary" text @click="submit">Adicionar Restaurante</v-btn>
-     
-     </v-form>
+        <v-card-actions>
+          <v-col class="text-left">
+            <v-btn color="#f7c23e" dark @click="submit">Adicionar reserva</v-btn>
+          </v-col>  
+          <v-col class="text-right">
+            <v-btn color="#f7c23e" dark @click="reset">Limpar Informação</v-btn>
+          </v-col>
+        </v-card-actions>
+      </v-form>
     </v-card>
   </v-dialog>
 </template>
 
+<style>
+#text {
+  color: white;
+  text-shadow: 0px 0px 5px gray;
+}
+</style>
+
 <script>
 export default {
-name:"reservation",
+  name: "reservation",
 
-data:() => ({
+  data: () => ({
+    fromDateMenu: false,
+    fromDateVal: null,
+    name: "",
+    peopleNumber: "",
+    mealTime: ["Almoço", "Jantar"],
+    mealTimeValue: "",
 
-  fromDateMenu: false,
-  fromDateVal: null,
-  name:'',
-  peopleNumber:'',
-  mealTime:['Almoço','Jantar'],
-  mealTimeValue:'',
+    nameRules: [v => !!v || "Por favor preencha o nome"],
 
-   nameRules: [
-      v => !!v || 'Por favor preencha o nome',
+    peopleNumberRules: [v => !!v || "Por favor indique o número de convidados"],
+
+    mealTimeRules: [
+      v => !!v || "Por favor indique o tipo de refeição que deseja"
     ],
 
-    peopleNumberRules:[
-       v => !!v || 'Por favor indique o número de convidados',
-    ],
-
-     mealTimeRules:[
-       v => !!v || 'Por favor indique o tipo de refeição que deseja',
-    ],
-
-     mealDateRules:[
-       v => !!v || 'Por favor indique a data em que deseja a reserva',
+    mealDateRules: [
+      v => !!v || "Por favor indique a data em que deseja a reserva"
     ]
-}),
+  }),
 
-methods: {
-    submit () {
+  methods: {
+    submit() {
       if (this.$refs.form.validate()) {
+        this.dialog = false;
 
-  
-        
-  
-
-         this.dialog=false
-         
-         this.$store.commit("ADD_RESERVATION",{
-           id: this.$store.getters.getLastReservationId,
-           userId: this.$store.getters.getLoggedUserId,
-           restaurantId: this.$store.getters.restaurantInfo(parseInt(this.$route.params.id)).id,
-           name: this.name,
-           peopleNumber: this.peopleNumber,
-           mealTime: this.mealTimeSelection,
-           mealDate: this.fromDateVal,
-         })
-
+        this.$store.commit("ADD_RESERVATION", {
+          id: this.$store.getters.getLastReservationId,
+          userId: this.$store.getters.getLoggedUserId,
+          restaurantId: this.$store.getters.restaurantInfo(
+            parseInt(this.$route.params.id)
+          ).id,
+          name: this.name,
+          peopleNumber: this.peopleNumber,
+          mealTime: this.mealTimeSelection,
+          mealDate: this.fromDateVal
+        });
       }
     },
-     reset() {
+    reset() {
       this.$refs.form.reset();
+    }
+  },
+
+  computed: {
+    //this.date1 = new Date().toISOString().substr(0, 10)
+    fromDateDisp() {
+      return this.fromDateVal;
+      // format date, apply validations, etc. Example below.
+      // return this.fromDateVal ? this.formatDate(this.fromDateVal) : "";
     },
-
-},
-
-
-computed:{
-  //this.date1 = new Date().toISOString().substr(0, 10)
-fromDateDisp(){
-        return this.fromDateVal;
-        // format date, apply validations, etc. Example below.
-        // return this.fromDateVal ? this.formatDate(this.fromDateVal) : "";
-      },
-fromDateTime(){
-  return this.mealTimeSelection;
-},
-
-
-}
-
-
-}
-
-
-
-
-
+    fromDateTime() {
+      return this.mealTimeSelection;
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
