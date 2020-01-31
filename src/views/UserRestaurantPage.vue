bd76ea3edcc22ddac5bccee0aedf7e0a04df6178
Miguel Amaral <mikefirebun@gmail.com>
2020-01-31 01:14:07 +0000

    added the simple alert on  the delete button of the dish delete and  alert on sucess on edit restaurant

--- src/views/UserRestaurantPage.vue
+++ src/views/UserRestaurantPage.vue
@@ -221,12 +221,31 @@ export default {
 
         // delete dish method
         deleteItem(){
-            confirm('Tem acertza que deseja remover este prato?') &&
-            this.$store.commit("REMOVE_DISH",{
-            id: this.selected[0].id
-          })
-          //update List 
-          this.dishes = this.$store.getters.getRestaurantDishes
+
+            this.$fire({
+                title: 'Quer remover o Prato?',
+                text: "You won't be able to revert this!",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sim!'
+                }).then((result) => {
+                if (result.value) {
+                    this.$store.commit("REMOVE_DISH",{
+                    id: this.selected[0].id            
+                    })
+                    //update List 
+                    this.dishes = this.$store.getters.getRestaurantDishes
+                    this.$fire({
+                        title:'Prato removido!',                        
+                        type:'success'})
+                }})
+                                
+
+
+
+          
         },
         // return recommended text
         getRecommended(recommendedV){
